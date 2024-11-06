7141cc72ac616ab0ef2f1ecb2a93d9f8c6f33dcd
meupaiomeupai <111287354+meupaiomeupai@users.noreply.github.com>
2024-11-06 19:44:17 -0300

    informacoes.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,20 +1,21 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
-
-async function vizualizarInformacoesGlobais() {
+async function vizualizarInformacoesViolao() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'  // Use a URL de dados relevante para o violão
     const res = await fetch(url)
     const dados = await res.json()
-    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+
+    // Adaptando os dados para o violão
+    const pessoasAprendendoViolao = (dados.total_pessoas_aprendendo_violao / 1e9)  // Exemplo de dado sobre violão
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
-    const horas = parseInt(dados.tempo_medio)
-    const minutos = Math.round((dados.tempo_medio - horas) * 100)
-    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
+    const horas = parseInt(dados.tempo_medio_de_estudo)
+    const minutos = Math.round((dados.tempo_medio_de_estudo - horas) * 100)
+    const porcentagemAprendendoViolao = ((pessoasAprendendoViolao / pessoasNoMundo) * 100).toFixed(2)
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasAprendendoViolao} bilhões</span> estão aprendendo violão e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> estudando violão.<br>Isso significa que aproximadamente <span>${porcentagemAprendendoViolao}%</span> da população mundial está aprendendo violão.`
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
 }
 
-vizualizarInformacoesGlobais()
+vizualizarInformacoesViolao()
