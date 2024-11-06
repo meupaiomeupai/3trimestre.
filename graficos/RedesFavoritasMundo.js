9284f60d8b36d5f7f5b99bb5fb07078fa0f9b35f
meupaiomeupai <111287354+meupaiomeupai@users.noreply.github.com>
2024-11-06 19:39:03 -0300

    Violão.js

--- graficos/RedesFavoritasMundo.js
+++ graficos/RedesFavoritasMundo.js
@@ -1,27 +1,31 @@
-import { getCSS, criarGrafico, incluirTexto } from "./common.js"
+import { getCSS, criarGrafico, incluirTexto } from "./common.js";
 
-async function redesFavoritasMundo() {
-    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json'
-    const res = await fetch(url)
-    const dados = await res.json()
-    const redes = Object.keys(dados)
-    const valores = Object.values(dados)
+async function preferenciasViolao() {
+    // Simulando dados de preferência dos alunos
+    const dados = {
+        "Aulas presenciais": 40,
+        "Aulas online": 35,
+        "Autodidata": 25
+    };
+
+    const preferencias = Object.keys(dados);
+    const valores = Object.values(dados);
 
     const data = [
         {
             values: valores,
-            labels: redes,
+            labels: preferencias,
             type: 'pie',
             textinfo: 'label+percent'
         }
-    ]
+    ];
 
     const layout = {
         plot_bgcolor: getCSS('--bg-color'),
         paper_bgcolor: getCSS('--bg-color'),
         height: 700,
         title: {
-            text: 'Redes sociais que os usuários mais gostam',
+            text: 'Preferências dos alunos no aprendizado de Violão',
             x: 0,
             font: {
                 color: getCSS('--primary-color'),
@@ -35,11 +39,10 @@ async function redesFavoritasMundo() {
                 size: 16
             }
         }
-    }
+    };
 
-    criarGrafico(data, layout)
-
-    incluirTexto(`Embora o <span>Instagram</span> ocupe a quarta posição em termos de número total de usuários entre as redes sociais, destaca-se como a <span>preferida pelos usuários</span>. Supera até mesmo o <span>Facebook</span>, a plataforma com mais usuários, sendo a terceira opção mais apreciada pelos usuários. <br>Essa preferência evidencia a forte conexão e apreço que as pessoas têm pelo Instagram em comparação com outras redes sociais`)
+    criarGrafico(data, layout);
+    incluirTexto(`A maioria dos alunos prefere aprender violão por <span>Aulas presenciais</span>, com uma boa parcela optando por <span>Aulas online</span> ou sendo <span>Autodidatas</span>. Este gráfico mostra a divisão entre os métodos mais comuns para aprender violão.`);
 }
 
-redesFavoritasMundo()
+preferenciasViolao();
