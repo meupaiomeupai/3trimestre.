fa41a3f9ba023399c46119a41c8c0f24211ba049
meupaiomeupai <111287354+meupaiomeupai@users.noreply.github.com>
2024-11-06 19:50:06 -0300

    redesSociaisMinhaEscola.js

--- graficos/redesSociaisMinhaEscola.js
+++ graficos/redesSociaisMinhaEscola.js
@@ -1,27 +1,29 @@
-import { criarGrafico, getCSS, incluirTexto } from "./common.js"
+import { criarGrafico, getCSS, incluirTexto } from "./common.js";
 
-async function redesSociaisFavoritasMinhaEscola() {
-    const dadosLocaisString = localStorage.getItem('respostaRedesSociais')
+async function plataformasViolaoPreferidas() {
+    const dadosLocaisString = localStorage.getItem('preferenciasViolao')
+    
     if (dadosLocaisString) {
-        const dadosLocais = JSON.parse(dadosLocaisString)
-        processarDados(dadosLocais)
+        const dadosLocais = JSON.parse(dadosLocaisString);
+        processarDados(dadosLocais);
     } else {
-        const url = 'https://script.googleusercontent.com/macros/echo?user_content_key=rSe23zaQC7gOvWgFJbdtPaqh7ewsO5hQmusYOeqdorTRN8C25vVV3BicsPoS6HS3jnJY9NHhy_pNZj6prQdxDH3305Mro8vNm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPvESZ9fvnAeFWqfIvIacdoRZcVMZ-nDSydw9_0gseo2TN3y60rOTtwDBCYnKQf6yIqgf8yOzNfccjP633C9VnHmUmPZvRBJY9z9Jw9Md8uu&lib=MCARBaBtNBMHKiEwMeRap3j6V_G7SlGWF'
-        const res = await fetch(url)
-        const dados = await res.json()
-        localStorage.setItem('respostaRedesSociais', JSON.stringify(dados))
-        processarDados(dados)
+        const url = 'https://script.googleusercontent.com/macros/echo?user_content_key=rSe23zaQC7gOvWgFJbdtPaqh7ewsO5hQmusYOeqdorTRN8C25vVV3BicsPoS6HS3jnJY9NHhy_pNZj6prQdxDH3305Mro8vNm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPvESZ9fvnAeFWqfIvIacdoRZcVMZ-nDSydw9_0gseo2TN3y60rOTtwDBCYnKQf6yIqgf8yOzNfccjP633C9VnHmUmPZvRBJY9z9Jw9Md8uu&lib=MCARBaBtNBMHKiEwMeRap3j6V_G7SlGWF';
+        const res = await fetch(url);
+        const dados = await res.json();
+        localStorage.setItem('preferenciasViolao', JSON.stringify(dados));
+        processarDados(dados);
     }
 }
 
 function processarDados(dados) {
-    const redesSociais = dados.slice(1).map(redes => redes[1])
-    const contagemRedesSociais = redesSociais.reduce((acc, redesSociais) => {
-        acc[redesSociais] = (acc[redesSociais] || 0) + 1
-        return acc
-    }, {})
-    const valores = Object.values(contagemRedesSociais)
-    const labels = Object.keys(contagemRedesSociais)
+    const plataformasViolao = dados.slice(1).map(violao => violao[1]); // Exemplo de plataformas de violão
+    const contagemPlataformas = plataformasViolao.reduce((acc, violao) => {
+        acc[violao] = (acc[violao] || 0) + 1;
+        return acc;
+    }, {});
+
+    const valores = Object.values(contagemPlataformas);
+    const labels = Object.keys(contagemPlataformas);
 
     const data = [
         {
@@ -30,14 +32,14 @@ function processarDados(dados) {
             type: 'pie',
             textinfo: 'label+percent'
         }
-    ]
+    ];
 
     const layout = {
         plot_bgcolor: getCSS('--bg-color'),
         paper_bgcolor: getCSS('--bg-color'),
         height: 700,
         title: {
-            text: 'Redes sociais que as pessoas da minha escola mais gostam',
+            text: 'Plataformas de Violão Mais Populares',
             x: 0,
             font: {
                 color: getCSS('--primary-color'),
@@ -51,10 +53,10 @@ function processarDados(dados) {
                 size: 16
             }
         }
-    }
+    };
 
-    criarGrafico(data, layout)
-    incluirTexto(`Como no mundo, a amostra de pessoas entrevistadas por mim, demonstra um apreço pelo <span>Instagram</span> em relação a outras redes.`)
+    criarGrafico(data, layout);
+    incluirTexto(`Assim como no mundo, as plataformas de violão mais populares entre os estudantes da nossa escola são <span> YouTube </span> e <span> cursos online </span>.`)
 }
 
-redesSociaisFavoritasMinhaEscola()
+plataformasViolaoPreferidas();
